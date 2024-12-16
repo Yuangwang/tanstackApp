f41c42862a5f2d94f242f3165bdb7af6d8ecd379
yuangwang <yuangwang2@gmail.com>
2024-12-16 17:06:41 -0500

    add app hosting preset

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'firebase-app-hosting',
+  },
   vite: {
     plugins: [
       tsConfigPaths({
